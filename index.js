2a2d32c14e0d16db80004ebe7b266cf73de36d3c
Jozef Butko <jbutko@gmail.com>
2017-04-05 07:31:23 +0200

    fix for updating dates

--- index.js
+++ index.js
@@ -9,49 +9,49 @@
 // public
 module.exports = function(schema) {
 
-	/**
-	 * Patch like update of model instance
-	 */
-	schema.statics.patchUpdate = function patchUpdate(query, updateData, protectedKeys, selectedKeys, cb) {
-		query = query || {};
-		updateData = updateData || {};
-		protectedKeys = protectedKeys || [];
-		selectedKeys = selectedKeys || '';
-		if (!Array.isArray(protectedKeys)) return 'protectedKeys must be of type array';
+  /**
+   * Patch like update of model instance
+   */
+  schema.statics.patchUpdate = function patchUpdate(query, updateData, protectedKeys, selectedKeys, cb) {
+    query = query || {};
+    updateData = updateData || {};
+    protectedKeys = protectedKeys || [];
+    selectedKeys = selectedKeys || '';
+    if (!Array.isArray(protectedKeys)) return 'protectedKeys must be of type array';
 
-		return this
-			.findOne(query)
-			.select(selectedKeys)
-			.exec()
-			.then((modelInstance) => {
-				if (!modelInstance) throw new Error('instanceNotFound');
+    return this
+      .findOne(query)
+      .select(selectedKeys)
+      .exec()
+      .then((modelInstance) => {
+        if (!modelInstance) throw new Error('instanceNotFound');
 
-				for (var attribute in updateData) {
-					let isProtectedKey = protectedKeys.includes(attribute);
-					let keyIsArray = Array.isArray(updateData[attribute]);
-					let isObjectKey = typeof modelInstance[attribute] == 'object' && !keyIsArray;
+        for (var attribute in updateData) {
+          let isProtectedKey = protectedKeys.includes(attribute);
+          let keyIsArray = Array.isArray(updateData[attribute]);
+          let isObjectWithKeys = typeof modelInstance[attribute] == 'object' && !keyIsArray && Object.keys(modelInstance[attribute]).length;
 
-					let keyIsUpdatable = updateData.hasOwnProperty(attribute) && attribute !== '_id' && !isProtectedKey && !isObjectKey;
-					let isNestedKey = updateData.hasOwnProperty(attribute) && attribute !== '_id' && !isProtectedKey && isObjectKey;
+          let keyIsUpdatable = updateData.hasOwnProperty(attribute) && attribute !== '_id' && !isProtectedKey && !isObjectWithKeys;
+          let isNestedKey = updateData.hasOwnProperty(attribute) && attribute !== '_id' && !isProtectedKey && isObjectWithKeys;
 
-					if (keyIsUpdatable)
-						modelInstance[attribute] = updateData[attribute];
-					else if (isNestedKey) {
-						for (let nestedAttribute in updateData[attribute]) {
-							let isProtectedNestedKey = protectedKeys.includes(`${attribute}.${nestedAttribute}`);
-							if (!isProtectedNestedKey)
-								modelInstance[attribute][nestedAttribute] = updateData[attribute][nestedAttribute];
-						}
-					}
-				}
+          if (keyIsUpdatable)
+            modelInstance[attribute] = updateData[attribute];
+          else if (isNestedKey) {
+            for (let nestedAttribute in updateData[attribute]) {
+              let isProtectedNestedKey = protectedKeys.includes(`${attribute}.${nestedAttribute}`);
+              if (!isProtectedNestedKey)
+                modelInstance[attribute][nestedAttribute] = updateData[attribute][nestedAttribute];
+            }
+          }
+        }
 
-				if (cb) return modelInstance.save(cb);
+        if (cb) return modelInstance.save(cb);
 
-				return modelInstance.save();
-			})
-			.catch((err) => {
-				if (cb) return cb({ message: err, status: 400 }, null);
-				return { message: err, status: 400 };
-			});
-	};
+        return modelInstance.save();
+      })
+      .catch((err) => {
+        if (cb) return cb({ message: err, status: 400 }, null);
+        return { message: err, status: 400 };
+      });
+  };
 };
